10c524353ae9460d5f29de7d2018ac56d1b4e837
devilmole <devilmole@gmail.com>
2016-10-14 14:42:10 +0800

    测试oschina

--- src/main/java/org/devilmole/controller/ShiroController.java
+++ src/main/java/org/devilmole/controller/ShiroController.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.devilmole.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Administrator on 2016/10/14 0014.
@@ -17,5 +18,9 @@ public class ShiroController {
 
     private static Logger logger = LogManager.getLogger(ShiroController.class);
 
+    @RequestMapping("/shiro/index")
+    public String shiroIndex(){
+        return "index";
+    }
 
 }
